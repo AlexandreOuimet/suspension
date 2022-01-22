a5fb9f35970970dea3ad54bdc0e5b3c245cc6d28
Alexandre Ouimet <alexandreouimet38@gmail.com>
2022-01-22 12:51:20 -0500

    delete de fichiers et début du timeline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+      <v-timeline-item small fill-dot>
+        <span slot="opposite">2021-08-03</span>
+        <v-card>
+          <v-card-text>
+            La suspension était réactive, le devant collait, piste en bonne
+            condition, beaucoup de confiance
+          </v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
 </template>
-
-<script>
-import HelloWorld from "../components/HelloWorld";
-
-export default {
-  name: "Home",
-
-  components: {
-    HelloWorld,
-  },
-};
-</script>
