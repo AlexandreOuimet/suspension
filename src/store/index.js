30b2780bc183f6109c0f4b5c324873f095bfc457
Alexandre Ouimet <alexandreouimet38@gmail.com>
2022-02-08 10:01:20 -0500

    modif de UI plus relié à ce que je veux

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,45 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import pathify from "./pathify";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+const store = new Vuex.Store({
+  state: {
+    setups: [
+      {
+        title: "Ste-Élie setup",
+        description: "Un setup de ste-élie, la piste était belle et sèche",
+        favorite: false,
+        fork: {
+          forkRebound: 10,
+          forkCompression: 18,
+        },
+        shock: {
+          shockRebound: 4,
+          shockLowComp: 2.5,
+          shockHighComp: 3,
+        },
+      },
+      {
+        title: "Ste-Julie setup",
+        description: "Un setup de ste-julie, la piste était belle et sèche",
+        favorite: true,
+        fork: {
+          forkRebound: 8,
+          forkCompression: 12,
+        },
+        shock: {
+          shockRebound: 8,
+          shockLowComp: 2,
+          shockHighComp: 1,
+        },
+      },
+    ],
+  },
   modules: {},
+
+  plugins: [pathify.plugin],
 });
+
+export default store;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,115 +1,40 @@
 <template>
   <v-container>
-    <v-timeline dense>
-      <v-timeline-item
-        v-for="n in 8"
-        :key="n"
-        small
-        @click.native.stop="onItemClicked()"
-      >
-        <v-card>
-          <v-card-title>
-            St-Élie
-            <v-spacer />
-            <span class="text-caption"> 2021-08-03 </span>
-          </v-card-title>
-          <v-card-text>
-            La suspension était réactive, le devant collait, piste en bonne
-            condition, beaucoup de confiance
-          </v-card-text>
-        </v-card>
-      </v-timeline-item>
-    </v-timeline>
+    <v-list two-line>
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <v-list-item v-for="(setup, i) in setups" :key="i">
+          <v-list-item-content>
+            <v-list-item-title>{{ setup.title }}</v-list-item-title>
+            <v-list-item-subtitle>{{ setup.description }}</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-icon v-if="setup.favorite" color="yellow">mdi-star</v-icon>
+            <v-icon v-else>mdi-star-plus-outline</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
 
-    <v-dialog
-      v-model="showSetupDialog"
-      fullscreen
-      hide-overlay
-      transition="dialog-bottom-transition"
-    >
-      <v-card>
-        <v-toolbar color="primary">
-          <v-btn icon @click="showSetupDialog = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-
-          <v-spacer />
-
-          <v-toolbar-items>
-            <v-btn icon>
-              <v-icon>mdi-trash-can-outline</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-check</v-icon>
-            </v-btn>
-          </v-toolbar-items>
-        </v-toolbar>
-
-        <v-tabs v-model="tab" fixed-tabs>
-          <v-tab>Front</v-tab>
-          <v-tab>Rear</v-tab>
-
-          <v-tabs-items v-model="tab" class="pt-4">
-            <!-- Front -->
-            <v-tab-item>
-              <circular-slider max="22" title="Rebound"></circular-slider>
-              <circular-slider max="22" title="Compression"></circular-slider>
-
-              <v-row class="justify-center mt-6">
-                <v-col cols="8">
-                  <v-text-field
-                    label="Air Pressure"
-                    filled
-                    dense
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-
-              <v-row class="justify-center">
-                <v-col cols="8">
-                  <v-text-field label="Spring Rate" filled dense></v-text-field>
-                </v-col>
-              </v-row>
-            </v-tab-item>
-
-            <!-- Rear -->
-            <v-tab-item>
-              <circular-slider
-                max="22"
-                title="Low Speed Compression"
-              ></circular-slider>
-              <circular-slider
-                max="22"
-                title="High Speed Compression"
-              ></circular-slider>
-
-              <circular-slider max="22" title="Rebound"></circular-slider>
-
-              <v-row class="justify-center">
-                <v-col cols="8">
-                  <v-text-field label="Shock" filled dense></v-text-field>
-                </v-col>
-              </v-row>
-            </v-tab-item>
-          </v-tabs-items>
-        </v-tabs>
-      </v-card>
-    </v-dialog>
+    <v-btn fab fixed bottom right color="primary">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
   </v-container>
 </template>
 
 <script>
-import CircularSlider from "../components/CircularSlider.vue";
+import { sync } from "vuex-pathify";
+
 export default {
-  components: { CircularSlider },
+  components: {},
   data: () => ({
-    showSetupDialog: false,
-    tab: null,
+    selectedItem: null,
   }),
 
+  computed: { setups: sync("setups") },
+
   methods: {
-    onItemClicked() {
-      this.showSetupDialog = true;
+    log() {
+      console.log("Logging : ", this.setups);
     },
   },
 };
