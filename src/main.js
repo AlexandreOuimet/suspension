e085104f576bdaa4645cda0b06ff1997fa9591e6
Alexandre Ouimet <alexandreouimet38@gmail.com>
2023-01-03 14:33:56 -0500

    Ajout des dépendances pour firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
+import firebase from "firebase";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYE1Zd22s8PaJzYOr8176fxumFhpKtKV0",
+  authDomain: "suspension-application.firebaseapp.com",
+  projectId: "suspension-application",
+  storageBucket: "suspension-application.appspot.com",
+  messagingSenderId: "633314525409",
+  appId: "1:633314525409:web:0954498809473ef77a77a0",
+  measurementId: "G-T2872PG5T9",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
